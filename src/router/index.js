1cf149e5bb679f686d412a69279dc89a43791021
balajichary123K <balajichary2020@gmail.com>
2025-10-25 14:49:51 +0530

    login incomplete

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,34 @@
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
+import Profile from '@/views/Profile.vue'
+import Projects from '@/views/Projects.vue'
+import Task from '@/views/Task.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path:"/login",
+      component:Login
+    },
+    {
+      path:"/Home",
+      component:Home
+    },
+    {
+      path:"/Profile",
+      component:Profile,
+    },
+    {
+      path:"/Projects",
+      component:Projects,
+    },
+    {
+      path:"/Task",
+      component:Task,
+    },
+  ],
 })
 
 export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bg-[url('https://i.pinimg.com/1200x/81/a5/ac/81a5acc0c27fe29a0a7515e839caa851.jpg')] bg-no-repeat bg-cover bg-center h-screen w-screen flex items-center justify-end">
+        <div class="bg-red-500">
+            
+        </div>
+    </div>
+</template>
+<script setup>
+</script>
